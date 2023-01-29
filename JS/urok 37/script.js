3cfc025fd2d3486b91b9e3d02c5e4e5a93d0a9b4
Klarajbk <klara.jalilbekova.ch@gmail.com>
2023-01-30 02:59:38 +0600

    37 урок

---
+++ JS/urok 37/script.js
@@ -0,0 +1,69 @@
+"use strict";
+// урок 35
+
+
+// использование строго режима для обработки данных
+
+// ООП - понятие 
+
+// Применение 
+
+// Как понять что есть строка
+
+let str = "текст";
+let strObj = new String(str);
+
+// console.log(str);       //выводит: текст
+// console.log(strObj);    // выводит: [String: 'текст']
+
+// console.log(typeof(str));       //string
+// console.log(typeof(strObj));    //object
+
+
+
+
+let car = {
+    kuzov: "Железо",
+    kolesa: "4 колеса",
+    modal: function() {
+            alert("Hello");
+    }
+};
+
+// let mb = {
+//     marka: "Мерс",
+//     god: "2023"
+// };
+
+
+let mb = Object.create(car);
+
+//mb._proto_ = car;
+
+//console.log(mb.kuzov);
+// console.log(mb.modal);
+
+
+//Object.setPrototypeOf(mb, car);
+console.log(mb.modal);
+mb.modal();
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
+
